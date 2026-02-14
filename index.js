730f0a33f165521d2c2b2312a6db9f96cb14ed3d
ali <ali69571@gmail.com>
2026-02-14 20:28:41 +0400

    Ez questions are done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // Q1 Print  NUmbers  from 1 to N:
 
 
-console.log("q1")
+console.log("----------------Question 1----------------")
 
 
 const num =parseInt(10);
@@ -17,7 +17,7 @@ console.log(i)
 
 // Q2:Print Number from N to 1
 
-console.log("q2")
+console.log("----------------Question 2----------------")
 
 
 for(let i =num;i>=0;i--){
@@ -27,7 +27,7 @@ console.log(i)
 
 
 // q3: Print number from n to 1 using same loop condition as q1
-console.log("q3")
+console.log("----------------Question 3----------------")
 
 for(let i =0;i<=num;i++){
 console.log(num-i)
@@ -35,7 +35,7 @@ console.log(num-i)
 
 
 // q4: Print all even numbers from 1 to N
-console.log("q4")
+console.log("----------------Question 4----------------")
 
 //we can use the same variable num
  
@@ -51,6 +51,9 @@ for(let i=0;i<=num;i++){
 
 //q5:Sum of N natural numbers:
 //we can use same num var.
+//Time complexcity is Big 0(n)
+//for this you can use the formula directly it will be much faster.
+console.log("----------------Question 5----------------")
 let sum=0;
 for(let i=1;i<=num;i++){
   
@@ -58,3 +61,47 @@ for(let i=1;i<=num;i++){
     
 }
 console.log("reuslt is",sum)
+
+
+//q6: Find factorial of N number:
+//we dont use num since its value is 10. we hardcode the 5 value here.
+console.log("----------------Question 6----------------")
+let factorial=1
+for(let i=5;i>=1;i--){
+
+    factorial= factorial*i
+    // 1=1*5=5
+    // 5=5*4=20
+    // 20=20*3=60
+    // 60=60*2=120
+    // 120=120*1=120
+   
+}
+console.log("factorial of num is: ",factorial)
+
+
+
+// q7: Sum of all even numbers
+
+console.log("----------------Question 7----------------")
+
+//we can use same num var here
+let summ=0;
+for(let i=1;i<=num;i++){
+
+if(i%2==0){
+console.log(i)
+summ=summ+i
+
+}
+console.log("result is",summ)
+}
+
+
+//q8: Print square of numbers from 1 to N
+//we can use same num var
+console.log("----------------Question 8----------------")
+
+for(let i=0;i<=num;i++){
+   console.log(`square of ${i} is ${i*i}`)
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+These solutions are in JS(Ecma Script)
+You can take the questions and implement it in any language.
+The core logics can vary depending on how you are trying to achieve the solutions.
+
+I am just polishing my skills and while i do that, i wanted to also polish recap the based fundamentals of programming. 
+Hence i am starting from the beggining, I am posting this here for anything that would find this useful. I have on purpose tried to make it beginner friendly.
